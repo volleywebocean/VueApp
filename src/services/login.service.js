76d44fc4b8a3abe68af14d7affd7e1421528fc1d
volleywebocean <volleysport1234@outlook.com>
2019-03-25 20:22:11 +0300

    change file

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -1,10 +1,11 @@
 import firebase from 'firebase'
-// import router from '@/router'
 export default {
     login(email, password){
         return firebase
       .auth()
+
       .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      
       .then((data) => {
          return firebase
           .auth()
